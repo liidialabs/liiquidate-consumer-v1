8bb3ee41ebc3aea95e87642cbc93bc151cb32835
calebomondi <kalebmokua@gmail.com>
2026-02-15 13:51:43 +0300

    Added accounting mapping, addresses and callCounts

--- src/FlashLoanRouter.sol
+++ src/FlashLoanRouter.sol
@@ -6,86 +6,100 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FlashLoanRouter is Ownable {
 
-    //
-    bytes32 public constant DEFAULT_PROVIDER_ID = keccak256("AAVE_V3");
-
-    // providerId => provider address
     mapping(bytes32 => address) public providers;
+    mapping(address => uint256) private debtCovered;
+    mapping(address => bool) private isRecorded;
 
-    // asset => ordered provider IDs
-    bytes32[] public providerPriority;
+    bytes32[] private providerPriority;
+    address[] private debts;
+    uint256 private callCount;
 
     event ProviderAdded(bytes32 id, address provider);
     event ProviderRemoved(bytes32 id);
     event ProviderPrioritySet(bytes32[] providers);
-    event FlashLoanRoutedOn(bytes32 providerId);
+    event FlashLoanRoutedOn(bytes32 provider);
     event FlashLoanExecuted(bytes32 provider);
+    event FlashLoanFailed(bytes32 provider);
 
-    constructor() Ownable(msg.sender) {}
+    constructor() Ownable(msg.sender) {
+        callCount = 0;
+    }
 
     function addProvider(address provider) external onlyOwner {
+        require(provider != address(0), "Invalid Address");
         bytes32 id = IFlashLoan(provider).id();
         providers[id] = provider;
         emit ProviderAdded(id, provider);
     }
 
     function removeProvider(bytes32 id) external onlyOwner {
+        require(id != bytes32(0), "Invalid Address");
         delete providers[id];
         emit ProviderRemoved(id);
     }
 
+    /// @notice Set UniswapV4 as first priority due to zero fees
     function setProviderPriority(
-        string[] calldata providerIds
+        bytes32[] calldata providerIds
     ) external onlyOwner {
-        require(providerIds.length > 0, "empty list");
-        
-        bytes32[] memory convertedIds = new bytes32[](providerIds.length);
-        for (uint256 i = 0; i < providerIds.length; i++) {
-            convertedIds[i] = keccak256(abi.encodePacked(providerIds[i]));
-        }
-        
-        providerPriority = convertedIds;
-        emit ProviderPrioritySet(convertedIds);
+        require(providerIds.length > 0, "empty list"); 
+
+        providerPriority = providerIds;
+
+        emit ProviderPrioritySet(providerIds);
     }
 
     function flashLoan(
         address debtAsset,
-        address collateralAsset
+        address collateralAsset,
         uint256 debtToCover,
         address targetContract,
         bytes calldata data
-    ) external {
-        if(providerPriority.length == 0) {
-            address provider = providers[DEFAULT_PROVIDER_ID];
-            if (provider == address(0)) return;
-            IFlashLoan(provider).flashLoan(
-                debtAsset,
-                collateralAsset
-                debtToCover,
-                targetContract,
-                data
-            );
-            return;
-        }
+    ) external returns(bool) {
+        require(providerPriority.length > 0, "Provider Priority Not Set");
 
+        callCount++;
+
+        if(!isRecorded[debtAsset]) debts.push(debtAsset);
+        debtCovered[debtAsset] += debtToCover;
+
+        // ...
         for (uint256 i = 0; i < providerPriority.length; i++) {
             address provider = providers[providerPriority[i]];
             if (provider == address(0)) continue;
 
             try IFlashLoan(provider).flashLoan(
                 debtAsset,
-                collateralAsset
+                collateralAsset,
                 debtToCover,
                 targetContract,
                 data
             ) {
                 emit FlashLoanExecuted(providerPriority[i]);
-                return; // success
+                return true; // success
             } catch {
                 emit FlashLoanRoutedOn(providerPriority[i]);
             }
         }
 
         revert("all flash loan providers failed");
+    }
+
+    ///////// VIEW //////////
+
+    function getCallCount() external view returns(uint256) {
+        return callCount;
+    }
+
+    function getProviderPriority() external view returns(bytes32[] memory list) {
+        list = providerPriority;
+    }
+
+    function getDebtsCovered() external view returns(address[] memory list) {
+        list = debts;
+    }
+
+    function getDebtsCoveredAmount(address debt) external view returns(uint256 amount) {
+        amount = debtCovered[debt];
     }
 }
