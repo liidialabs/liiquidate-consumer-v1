dc2e52d0b006906623a660c721c7ca40a85b80f1
calebomondi <kalebmokua@gmail.com>
2026-03-03 13:02:03 +0300

    added natspec

--- script/1a_DeployScript.s.sol
+++ script/1a_DeployScript.s.sol
@@ -12,22 +12,31 @@ import {UniswapV4Adapter} from "../src/swappers/UniswapV4Adapter.sol";
 import {UniswapV4} from "../src/flashloans/UniswapV4.sol";
 import {AaveV3} from "../src/flashloans/AaveV3.sol";
 
+/// @title DeployScript
+/// @notice Deploys all core contracts for the Liiquidate system
+/// @dev Deploys AdapterRegistry, FlashLoanRouter, UniversalSwapRouter, Liiquidate,
+///      UniswapV4Adapter, and flash loan providers (UniswapV4, AaveV3)
 contract DeployScript is Script {
-    // Helper
+    /// @notice Helper configuration contract
     HelperConfig helperConfig;
-    // Main Contracts
+    /// @notice Registry for liquidation adapters
     AdapterRegistry adapterRegistry;
+    /// @notice Router for flash loan execution
     FlashLoanRouter flashRouter;
+    /// @notice Router for token swaps
     UniversalSwapRouter swapRouter;
+    /// @notice Main liquidation consumer contract
     Liiquidate liiquidate;
-    // Swap Adapter
+    /// @notice Uniswap V4 swap adapter
     UniswapV4Adapter uniswapAdapter;
-    // Flashloan
+    /// @notice Uniswap V4 flash loan provider
     UniswapV4 uniswapFlashloan;
+    /// @notice Aave V3 flash loan provider
     AaveV3 aaveV3Flashloan;
 
+    /// @notice Main deployment function
+    /// @dev Deploys all contracts and configures them with proper addresses and priorities
     function run() public {
-        // deploy helper config
         helperConfig = new HelperConfig();
 
         (
@@ -35,8 +44,6 @@ contract DeployScript is Script {
             address aaveV3PoolAddress,
             address uniswapV4PoolAddress
         ) = helperConfig.activeNetworkConfig();
-
-        // deploy
 
         vm.startBroadcast(helperConfig.deployerKey());
 
@@ -48,31 +55,24 @@ contract DeployScript is Script {
 
         ////// DEPLOY & REGISTER SWAP ADAPTER //////
 
-        // deploy
         uniswapAdapter = new UniswapV4Adapter(uniswapV4PoolAddress);
-        // register
         swapRouter.registerAdapter(address(uniswapAdapter));
-        // add to priority queu
         bytes32[] memory protocols = new bytes32[](1);
         protocols[0] = uniswapAdapter.protocolId();
         swapRouter.setProtocolPriority(protocols);
 
         ///// DEPLOY AND REGISTER FLASH LOAN ADAPTERS /////
 
-        // deploy uniswapV4
         uniswapFlashloan = new UniswapV4(
             uniswapV4PoolAddress,
             address(swapRouter)
         );
-        // deploy aaveV3
         aaveV3Flashloan = new AaveV3(
             aaveV3PoolAddress,
             address(swapRouter)
         );
-        // add providers 
         flashRouter.addProvider(address(uniswapFlashloan));
         flashRouter.addProvider(address(aaveV3Flashloan));
-        // set priority
         bytes32[] memory _protocols = new bytes32[](2);
         _protocols[0] = uniswapFlashloan.id();
         _protocols[1] = aaveV3Flashloan.id();
@@ -81,12 +81,12 @@ contract DeployScript is Script {
         ///// DEPLOY LIIQUIDATE AND SET AS PROXY /////
 
         liiquidate = new Liiquidate(
+            helperConfig.USDC(),
             address(adapterRegistry), 
             address(flashRouter), 
             forwarderAddress
         );
 
-        // Print addresses to stdout
         console2.log("AdapterRegistry:", address(adapterRegistry));
         console2.log("FlashLoanRouter:", address(flashRouter));
         console2.log("UniversalSwapRouter:", address(swapRouter));
@@ -95,7 +95,6 @@ contract DeployScript is Script {
         console2.log("UniswapV4Adapter:", address(uniswapAdapter));
         console2.log("UniswapV4FlashloanAdapter:", address(uniswapFlashloan));
         console2.log("AaveV3FlashloanAdapter:", address(aaveV3Flashloan));
-
 
         vm.stopBroadcast();
     }
